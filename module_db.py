c71e4787b2d083f91f84450ea24cfba3b57eb81f
中野雄貴 <totoro.windows@outlook.com>
2018-03-09 02:55:21 +0900

    データの追加(SQL)追加

--- module_db.py
+++ module_db.py
@@ -20,10 +20,50 @@ def returnData(data):
 
 # SQLiteクエリ実行
 def access(query, db_name):
+    print(query)
     connection = sqlite3.connect(db_name)
     cursor = connection.cursor()
-    result = cursor.execute(query).fetchall()
+    try:
+        result = cursor.execute(query).fetchall()
+    except sqlite3.IntegrityError:
+        cursor.close()
+        connection.close()
+        raise sqlite3.IntegrityError
+
     connection.commit()
     cursor.close()
     connection.close()
     return result
+
+# データ追加
+def add(data):
+    try:
+        access('''INSERT into main(line, station, type, value, rating_count) values({}, {}, {}, {}, {});'''.format(data[0], data[1], data[2], data[3], data[4]), 'db/main.db')
+        return True
+    except:
+        return False
+    
+
+def random_select():
+    pass
+
+def update():
+    pass
+
+# 初期化
+def init_main_db():
+    access('''CREATE TABLE main(
+        u_id INTEGER PRIMARY KEY,
+        line TEXT NOT NULL , 
+        station TEXT NOT NULL, 
+        type TEXT NOT NULL, 
+        value TEXT NOT NULL,
+        rating_count INTEGER NOT NULL);'''
+    , 'db/main.db')
+
+if __name__ == '__main__':
+    # init_main_db()
+    data = ['"橿原線"','"近鉄郡山駅"','"text"','"古い"', '1']
+    add(data)
+
+# u_id INTEGER PRIMARY KEY, 
