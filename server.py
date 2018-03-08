8133197673e9cb452e7760d8dded272484eb46d7
中野雄貴 <totoro.windows@outlook.com>
2018-03-09 02:55:21 +0900

    データの追加(SQL)追加

--- server.py
+++ server.py
@@ -1,8 +1,22 @@
 from bottle import *
+from module import *
+import json
+from module_db import *
+from linebot import webhook
+import setting
+import main
 
 @post('/')
-def top():
-    return ''' Hello World! '''
+def line_post():
+    signature = request.get_header('X-Line-Signature')
+    body = request.body.read().decode('utf-8')
+
+    events = webhook.WebhookParser(setting.CHANNEL_SECRET).parse(body, signature)
+    main.main(events)
+
+    body = json.dumps({})
+    r = HTTPResponse(status=200, body=body)
+    r.set_header('Content-Type', 'application/json')
 
 @get('/')
 def index():
@@ -11,6 +25,52 @@ def index():
 @get('/<name>')
 def tekito(name):
     return name
-    
 
-run(host='0.0.0.0', port=4490, debug=True)
+# API
+# 駅情報
+@post('/api/station')
+def postStationData():
+    json_data = [request.json['LocalCode'], request.json['LineCode'], request.json['CodeStation']]
+    print(json_data)
+    return module_db.returnData(json_data)
+
+# データベースに情報登録
+@post('/api/register')
+def postRegistData():
+    try:
+        json_data = ['"{}"'.format(request.json['line']), '"{}"'.format(request.json['station']), '"{}"'.format(request.json['type']), '"{}"'.format(request.json['value']), '"{}"'.format(request.json['rating_count'])] 
+    except:
+        return 'failure'
+    if add(json_data):
+        return 'success'
+    else:
+        return 'failure'
+    
+# PATH設定
+# 送られてきた画像を保存
+@route('/img/<filename>')
+def route_img(filename):
+    return static_file(filename, root='contents/user/img')
+
+@route('/css/<filename>')
+def route_css(filename):
+    return static_file(filename, root='css/', mimetype='text/css')
+
+@route('/js/<filename>')
+def route_js(filename):
+    return static_file(filename, root='js/', mimetype='text/javascript')
+
+@route('/fonts/<filename>')
+def route_fonts(filename):
+    return static_file(filename, root='fonts')
+
+@route('/images/<filename>')
+def route_images(filename):
+    return static_file(filename, root='views/images/')
+
+@route('/db/<filename>')
+def route_db(filename):
+    return static_file(filename, root='db/')
+
+run(host='0.0.0.0', port=4490, debug=True, reloader=True)
+
