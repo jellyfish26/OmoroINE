9f1262c27fabe6c66799b0e5a935f4eb54fddf25
jellyfish26 <jellyfish026@outlook.jp>
2018-03-09 03:33:23 +0900

    KeyStoreSystem

--- android/app/src/main/java/joken/ac/jp/omoroic/MainActivity.kt
+++ android/app/src/main/java/joken/ac/jp/omoroic/MainActivity.kt
@@ -1,12 +1,69 @@
 package joken.ac.jp.omoroic
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.NfcF
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var intentFiltersArray: Array<IntentFilter>? = null
+    private var techListsArray: Array<Array<String>>? = null
+    private var mAdapter: NfcAdapter? = null
+    private var pendingIntent: PendingIntent? = null
+    private val nfcReader = NfcReader()
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+
+        try
+        {
+            ndef.addDataType("text/plain")
+        }
+        catch (e: IntentFilter.MalformedMimeTypeException)
+        {
+            throw RuntimeException("fail", e)
+        }
+
+        intentFiltersArray = arrayOf(ndef)
+
+        techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
+
+        // NfcAdapterを取得
+        mAdapter = NfcAdapter.getDefaultAdapter(applicationContext)
+
+    }
+
+    override fun onResume()
+    {
+        super.onResume()
+        // NFCの読み込みを有効化
+        mAdapter?.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
+    }
+
+    override fun onNewIntent(intent: Intent)
+    {
+        // IntentにTagの基本データが入ってくるので取得。
+        val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG) ?: return
+
+        nfcReader.readTag(tag)
+    }
+
+    override fun onPause()
+    {
+        super.onPause()
+        mAdapter?.disableForegroundDispatch(this)
     }
 }
