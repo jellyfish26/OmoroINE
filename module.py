d85a90429a1c4d6ac0eb49fab91b1c23f33fb90e
中野雄貴 <totoro.windows@outlook.com>
2018-03-09 05:26:55 +0900

    ServerとLINE bot

--- module.py
+++ module.py
@@ -1,4 +1,6 @@
 import json, requests, uuid
+from PIL import Image
+import numpy as np
 
 # 位置情報から線と駅情報を取得
 def isStation(position):
@@ -8,9 +10,9 @@ def isStation(position):
     out = []
     # 駅名と線情報を取得
     # リストに書き込み
-    for station in response['response']['station']:[
-        out.append((station['line'], station['name']))
-    ]
+    # for station in response['response']['station']:
+    #     out.append((station['line'], station['name']))
+    out = [(station['line'], station['name']) for station in response['response']['station']]
     return out
 
 # ユニークID発行
@@ -18,8 +20,22 @@ def issueUnique():
     uniqueId = uuid.uuid4()
     return uniqueId
 
+# Preview作成
 def GeneratPreview(img_url):
-    pass
+    try:
+        img = Image.open(img_url, 'r')
+        path_list = img_url.split('/')
+        if len(path_list) == 0:
+            file_name = path_list[0]
+        else:
+            file_name = path_list[len(path_list)-1]
+        extension_list = file_name.split('.')
+        extension = extension_list[len(extension_list)-1]
+        resize_img = img.resize((200, 200))
+        resize_img.save('contents/system/preview/{}'.format(file_name), extension, quality=100, optimize=True)
+    except:
+        return False
+    return True
 
 if __name__ == '__main__':
     position = [34.697164, 135.508032]
